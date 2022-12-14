283be68a6d080f808cc2094cfafafef88e108a5c
Michelle Santana <michelledearaujo2@gmail.com>
2022-12-14 20:28:18 -0300

    feat/animação

--- script.js
+++ script.js
@@ -1,102 +1,119 @@
 function carregaDicionario() {
-    let biografias = {
-        bio01: {
-            nome: "Marielle Franco",
+  let biografias = {
+    bio01: {
 
-            imagem: "./imagens/marielle-franco.png",
+      nome: "Angela Davis",
 
-            descricao: 
+      imagem: "./imagens/angela-davis.png",
 
-            "Marielle Francisco da Silva, conhecida como Marielle Franco, foi uma socióloga, feminista, militante dos direitos humanos e política brasileira. Filiada ao Partido Socialismo e Liberdade (PSOL), elegeu-se vereadora do Rio de Janeiro para a Legislatura 2017-2020, durante a eleição municipal de 2016, com a quinta maior votação. Crítica da intervenção federal no Rio de Janeiro e da Polícia Militar, denunciava constantemente abusos de autoridade por parte de policiais contra moradores de comunidades carentes. Em 14 de março de 2018, foi assassinada a tiros",
+      descricao:
 
-            citacao: 
-            
-            "Quantos mais vão precisar morrer para que essa guerra aos pobres acabe?"
-        },
-        bio02: {
-      
-            nome: "Carolina de Jesus",
-      
-            imagem: "./imagens/carolina-de-jesus.png",
-      
-            descricao:
-      
-              "Carolina Maria de Jesus foi uma escritora brasileira, conhecida por seu livro Quarto de Despejo: Diário de uma Favelada publicado em 1960. Considerada uma das primeiras e mais importantes escritoras negras do Brasil. A autora viveu boa parte de sua vida na favela do Canindé, na zona norte de São Paulo, sustentando a si mesma e seus três filhos como catadora de papéis. Em 1958 tem seu diário publicado sob o nome Quarto de Despejo, com auxílio do jornalista Audálio Dantas. O livro fez um enorme sucesso e chegou a ser traduzido para quatorze línguas.",
-      
-            citacao:
-      
-              "Ah, comigo o mundo vai modificar-se. Não gosto do mundo como ele é.",
-      
-        },
+        "Angela Yvonne Davis é uma professora e filósofa socialista estado-unidense que alcançou notoriedade mundial na década de 1970 como integrante do Partido Comunista dos Estados Unidos, dos Panteras Negras, por sua militância pelos direitos das mulheres e contra a discriminação social e racial nos Estados Unidos e por ser personagem de um dos mais polêmicos e famosos julgamentos criminais da recente história dos Estados Unidos.",
 
-          bio03: {
-      
-            nome: "Conceição Evaristo",
-      
-            imagem: "./imagens/conceicao-evaristo.png",
-      
-            descricao:
-      
-              "Maria da Conceição Evaristo de Brito nasceu em Belo Horizonte, em 1946. De origem humilde, migrou para o Rio de Janeiro na década de 1970. Graduada em Letras pela UFRJ, trabalhou como professora da rede pública de ensino da capital fluminense. É Mestre em Literatura Brasileira pela PUC do Rio de Janeiro, com a dissertação Literatura Negra: uma poética de nossa afro-brasilidade (1996), e Doutora em Literatura Comparada na Universidade Federal Fluminense, com a tese Poemas malungos, cânticos irmãos (2011), na qual estuda as obras poéticas dos afro-brasileiros Nei Lopes e Edimilson de Almeida Pereira em confronto com a do angolano Agostinho Neto.",
-      
-            citacao:
-      
-              "O importante não é ser o primeiro ou primeira, o importante é abrir caminhos.",
-      
-        },
+      citacao:
 
-        bio04: {
+        "Não aceito mais as coisas que não posso mudar, estou mudando as coisas que não posso aceitar.",
 
-            nome: "Lélia Gonzalez",
-      
-            imagem: "./imagens/lelia-gonzalez.png",
-      
-            descricao: "Lélia Gonzalez foi uma intelectual, política, professora e antropóloga brasileira. Ajudou a fundar instituições como o Movimento Negro Unificado (MNU), o Instituto de Pesquisas das Culturas Negras (IPCN), o Coletivo de Mulheres Negras N'Zinga e o Olodum. Sua militância em defesa da mulher negra levou-a ao Conselho Nacional dos Direitos da Mulher (CNDM), no qual atuou de 1985 a 1989. Foi candidata a deputada federal pelo PT, elegendo-se primeira suplente. Nas eleições seguintes, em 1986, candidatou-se a deputada estadual pelo PDT, novamente elegendo-se suplente.",
-      
-            citacao:
-      
-              "A gente não nasce negro, a gente se torna negro. É uma conquista dura, cruel e que se desenvolve pela vida da gente afora.",
-      
-        },
+    },
 
-        bio05: {
-      
-            nome: "Rosa Parks",
-      
-            imagem: "./imagens/rosa-parks.png",
-      
-            descricao:
-      
-              "Rosa Louise McCauley, mais conhecida por Rosa Parks, foi uma costureira negra norte-americana, símbolo do movimento dos direitos civis dos negros nos Estados Unidos. Ficou famosa, em 1º de dezembro de 1955, por ter-se recusado frontalmente a ceder o seu lugar no ônibus a um branco, tornando-se o estopim do movimento que foi denominado boicote aos ônibus de Montgomery e posteriormente viria a marcar o início da luta antissegregacionista.",
-      
-            citacao:
-      
-              "⁠Você nunca deve ter medo do que está fazendo quando está certo.",
-      
-        },
+    bio02: {
 
-        bio06: {
-      
-            nome: "Angela Davis",
-      
-            imagem: "./imagens/angela-davis.png",
-      
-            descricao:
-      
-              "Angela Yvonne Davis é uma professora e filósofa socialista estado-unidense que alcançou notoriedade mundial na década de 1970 como integrante do Partido Comunista dos Estados Unidos, dos Panteras Negras, por sua militância pelos direitos das mulheres e contra a discriminação social e racial nos Estados Unidos e por ser personagem de um dos mais polêmicos e famosos julgamentos criminais da recente história dos Estados Unidos.",
-      
-            citacao:
-      
-              "Não aceito mais as coisas que não posso mudar, estou mudando as coisas que não posso aceitar.",
-      
-        },   
-    };
+      nome: "Carolina de Jesus",
 
-    let content = document.getElementById("content");
+      imagem: "./imagens/carolina-de-jesus.png",
+
+      descricao:
+
+        "Carolina Maria de Jesus foi uma escritora brasileira, conhecida por seu livro Quarto de Despejo: Diário de uma Favelada publicado em 1960. Considerada uma das primeiras e mais importantes escritoras negras do Brasil. A autora viveu boa parte de sua vida na favela do Canindé, na zona norte de São Paulo, sustentando a si mesma e seus três filhos como catadora de papéis. Em 1958 tem seu diário publicado sob o nome Quarto de Despejo, com auxílio do jornalista Audálio Dantas. O livro fez um enorme sucesso e chegou a ser traduzido para quatorze línguas.",
+
+      citacao:
+
+        "Ah, comigo o mundo vai modificar-se. Não gosto do mundo como ele é.",
+
+    },
+
+    bio03: {
+
+      nome: "Conceição Evaristo",
+
+      imagem: "./imagens/conceicao-evaristo.png",
+
+      descricao:
+
+        "Maria da Conceição Evaristo de Brito nasceu em Belo Horizonte, em 1946. De origem humilde, migrou para o Rio de Janeiro na década de 1970. Graduada em Letras pela UFRJ, trabalhou como professora da rede pública de ensino da capital fluminense. É Mestre em Literatura Brasileira pela PUC do Rio de Janeiro, com a dissertação Literatura Negra: uma poética de nossa afro-brasilidade (1996), e Doutora em Literatura Comparada na Universidade Federal Fluminense, com a tese Poemas malungos, cânticos irmãos (2011), na qual estuda as obras poéticas dos afro-brasileiros Nei Lopes e Edimilson de Almeida Pereira em confronto com a do angolano Agostinho Neto.",
+
+      citacao:
+
+        "O importante não é ser o primeiro ou primeira, o importante é abrir caminhos.",
+
+    },
+
+    bio04: {
+
+      nome: "Lélia Gonzalez",
+
+      imagem: "./imagens/lelia-gonzalez.png",
+
+      descricao: "Lélia Gonzalez foi uma intelectual, política, professora e antropóloga brasileira. Ajudou a fundar instituições como o Movimento Negro Unificado (MNU), o Instituto de Pesquisas das Culturas Negras (IPCN), o Coletivo de Mulheres Negras N'Zinga e o Olodum. Sua militância em defesa da mulher negra levou-a ao Conselho Nacional dos Direitos da Mulher (CNDM), no qual atuou de 1985 a 1989. Foi candidata a deputada federal pelo PT, elegendo-se primeira suplente. Nas eleições seguintes, em 1986, candidatou-se a deputada estadual pelo PDT, novamente elegendo-se suplente.",
+
+      citacao:
+
+        "A gente não nasce negro, a gente se torna negro. É uma conquista dura, cruel e que se desenvolve pela vida da gente afora.",
+
+    },
+
+    bio05: {
+      nome: "Marielle Franco",
+
+      imagem: "./imagens/marielle-franco.png",
+
+      descricao:
+
+        "Marielle Francisco da Silva, conhecida como Marielle Franco, foi uma socióloga, feminista, militante dos direitos humanos e política brasileira. Filiada ao Partido Socialismo e Liberdade (PSOL), elegeu-se vereadora do Rio de Janeiro para a Legislatura 2017-2020, durante a eleição municipal de 2016, com a quinta maior votação. Crítica da intervenção federal no Rio de Janeiro e da Polícia Militar, denunciava constantemente abusos de autoridade por parte de policiais contra moradores de comunidades carentes. Em 14 de março de 2018, foi assassinada a tiros.",
+
+      citacao:
+
+        "Quantos mais vão precisar morrer para que essa guerra aos pobres acabe?"
+    },
+
+    bio06: {
+      nome: "Nina Da Hora",
+
+      imagem: "./imagens/nina-hora.png",
+
+      descricao:
+
+        "Ana Carolina Silva das Neves da Hora, a Nina da Hora, como é conhecida nas redes sociais, se tornou referência da nova geração de pesquisadores e cientistas ligados à tecnologia no País. A infância humilde na periferia de Duque de Caxias (RJ), longe das telas dos computadores, não a impediu de sonhar em trabalhar com computação e despontar como uma grande “hacker”. Pesquisadora do Centro de Tecnologia e Sociedade da Fundação Getúlio Vargas (CTS-FGV), colunista da revista MIT Technology Review, do tradicional instituto norte-americano, e integrante do Conselho de Segurança da rede social TikTok. Foi convidada para participar da Comissão de Transparência das Eleições no Tribunal Superior Eleitoral (TSE).",
+
+      citacao:
+
+        "Hackear, para mim, não está ligado só à computação, mas a hackear padrões, formatos, sempre em busca de algo mais coletivo e acessível"
+    },
+
+    bio07: {
+
+      nome: "Rosa Parks",
+
+      imagem: "./imagens/rosa-parks.png",
+
+      descricao:
+
+        "Rosa Louise McCauley, mais conhecida por Rosa Parks, foi uma costureira negra norte-americana, símbolo do movimento dos direitos civis dos negros nos Estados Unidos. Ficou famosa, em 1º de dezembro de 1955, por ter-se recusado frontalmente a ceder o seu lugar no ônibus a um branco, tornando-se o estopim do movimento que foi denominado boicote aos ônibus de Montgomery e posteriormente viria a marcar o início da luta antissegregacionista.",
+
+      citacao:
+
+        "⁠Você nunca deve ter medo do que está fazendo quando está certo.",
+
+    },
 
 
-    for (let bio in biografias){
-      content.innerHTML +=
+  };
+
+  let content = document.getElementById("content");
+
+
+  for (let bio in biografias) {
+    content.innerHTML +=
       '<div class="card">' +
       '<img src="' +
       biografias[bio].imagem +
@@ -108,13 +125,27 @@ function carregaDicionario() {
       '<p>' +
       biografias[bio].descricao +
       '</p>' +
-      '<blockquote><q>' +
+      '<blockquote>' +
+      "Citação: " +
+      '<q>' +
       biografias[bio].citacao +
       '</blockquote></q>'
-      '</details>' +
+    '</details>' +
       '</div>';
-    }
   }
-    
+}
+
 
 carregaDicionario();
+
+function typeWriter(element) {
+  const textoArray = element.innerHTML.split('');
+  element.innerHTML = '';
+  textoArray.forEach((letra, i) => {
+    setTimeout(() => element.innerHTML += letra, 75 * i);
+  });
+}
+
+
+const titulo = document.querySelector('h1');
+typeWriter(titulo);
